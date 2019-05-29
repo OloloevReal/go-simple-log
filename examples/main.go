613941baa3c880ca62e60c53fef121ceb9e55bbb
OloloevReal <nickey.n@gmail.com>
2019-05-30 01:28:55 +0300

    added caller info

--- examples/main.go
+++ examples/main.go
@@ -6,8 +6,13 @@ import (
 
 func main() {
 	slog := simlog.NewLogger()
-	slog.Printf("test log message")
+	slog.Printf("[ERROR] test log message")
 
-	simlog.SetOptions(simlog.SetDebug)
-	simlog.Printf("This is a [%s] log", "global")
+	simlog.Printf("This is a INFO [%s] log", "global")
+	//simlog.SetOptions(simlog.SetDebug)
+	simlog.SetOptions(simlog.SetDebug, simlog.SetCaller)
+	simlog.Printf("[DEBUG] This is a [%s] log", "global")
+
+	simlog.Fatalf("[FATAL] This is a fatal log")
+	//simlog.Panicf("[INFO] This is a panic log")
 }
