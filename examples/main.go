abc3c81260824763404856eb62ae866a0e6ca089
OloloevReal <nickey.n@gmail.com>
2019-05-29 11:14:30 +0300

    commit

--- examples/main.go
+++ examples/main.go
@@ -1,4 +1,12 @@
 package main
 
-func main(){
+import (
+	simlog "github.com/OloloevReal/go-simple-log"
+)
+
+func main() {
+	slog := simlog.NewLogger()
+	slog.Printf("test log message")
+
+	simlog.Printf("This is a [%s] log", "global")
 }
--- simlog.go
+++ simlog.go
@@ -1,5 +1,43 @@
 package simlog
 
+import "log"
+
 /*
 Just simple logs, not more
 */
+
+var version = "0.0.1"
+
+var (
+	infoTag  = "[INFO]"
+	debugTag = "[DEBUG]"
+	errorTag = "[ERROR]"
+)
+
+type Logger struct {
+	debug bool
+}
+
+type Option func(l *Logger)
+
+func SetDebug(l *Logger) {
+	l.debug = true
+}
+
+func NewLogger(opts ...Option) *Logger {
+	l := &Logger{}
+	for _, opt := range opts {
+		opt(l)
+	}
+	return l
+}
+
+func (l *Logger) Printf(format string, v ...interface{}) {
+	log.Printf(format, v...)
+}
+
+var globalLog = NewLogger()
+
+func Printf(format string, v ...interface{}) {
+	globalLog.Printf(format, v...)
+}
